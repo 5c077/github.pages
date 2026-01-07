707259f4a7c7801ef2381aabc9f14acdb77a99d4
5c077 <20480999+5c077@users.noreply.github.com>
2026-01-07 15:59:44 -0800

    config

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,20 +1,12 @@
 (function() {
     const script = document.createElement('script');
-    script.defer = true;
-    script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
-    script.setAttribute('data-cf-beacon', '{"token": "YOUR_TOKEN_HERE"}');
-    document.head.appendChild(script);
-})();
-(function() {
-    const script = document.createElement('script');
     script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${CONFIG.GA_MEASUREMENT_ID}`;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=G-C3ZJZTH2CW`;
     document.head.appendChild(script);
-
     script.onload = function() {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', CONFIG.GA_MEASUREMENT_ID);
+        gtag('config', 'G-C3ZJZTH2CW');
     };
 })();
