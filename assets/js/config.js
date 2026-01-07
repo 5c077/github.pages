83a35d3650fb10369d48cbf8ed2abd57c783cde8
5c077 <20480999+5c077@users.noreply.github.com>
2026-01-07 15:46:39 -0800

    config

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,3 +5,16 @@
     script.setAttribute('data-cf-beacon', '{"token": "YOUR_TOKEN_HERE"}');
     document.head.appendChild(script);
 })();
+(function() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${CONFIG.GA_MEASUREMENT_ID}`;
+    document.head.appendChild(script);
+
+    script.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', CONFIG.GA_MEASUREMENT_ID);
+    };
+})();
