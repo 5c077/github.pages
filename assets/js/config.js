f9e5a43fa013ccdbc9803f58323b49b2d7ad0bb8
5c077 <20480999+5c077@users.noreply.github.com>
2026-01-07 15:34:31 -0800

    config

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,13 +1,7 @@
 (function() {
     const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX';
+    script.defer = true;
+    script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
+    script.setAttribute('data-cf-beacon', '{"token": "YOUR_TOKEN_HERE"}');
     document.head.appendChild(script);
-
-    script.onload = function() {
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-XXXXXXXXXX');
-    };
 })();
